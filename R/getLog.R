bf49e94b4a37d6a1cc4c0b1b08032e038bee0786
Siddarta Jairam <sidjsb@gmail.com>
2015-09-12 15:18:19 -0700

    space -> tabs

--- R/getLog.R
+++ R/getLog.R
@@ -10,125 +10,125 @@
 #' 	table version of the numeric stats in $table other raw stats a
 #' @export
 getLog <- function(
-  logId,
-  altNames = c(),
-  keepClassSpec = FALSE,
-  keepChat = FALSE){
+	logId,
+	altNames = c(),
+	keepClassSpec = FALSE,
+	keepChat = FALSE){
 
 
-  matchUrl <- paste0("http://logs.tf/json/", logId)
+	matchUrl <- paste0("http://logs.tf/json/", logId)
 
-  niceMatch <- match <- jsonlite::fromJSON(matchUrl)
+	niceMatch <- match <- jsonlite::fromJSON(matchUrl)
 
-  killInd <- grep("version", names(match))
-  if(!keepChat) killInd <- c(killInd, grep("chat", names(match)))
-  if(!keepClassSpec) killInd <- c(killInd, grep("class", names(match)))
+	killInd <- grep("version", names(match))
+	if(!keepChat) killInd <- c(killInd, grep("chat", names(match)))
+	if(!keepClassSpec) killInd <- c(killInd, grep("class", names(match)))
 
-  niceMatch <- niceMatch[-killInd]
-
-
-
-  plyMat <- t(vapply(niceMatch$players,function(x){
-    cleanLogMat(x, teamNames = names(match$teams))
-  },rep(1.1, 27)))
+	niceMatch <- niceMatch[-killInd]
 
 
-  cleanUpVec <- plyMat[, "dmg_real"] / plyMat[, "dmg"]
-  dmgPerVec <- plyMat[, "dmg"] / plyMat[, "hr"]
-  dmgPerVec[is.infinite(dmgPerVec)] <- NA #Medics don't get heals
 
-  totHeal <- rep(0,2)
-  medSet <- names(match$healspread)
-  totHeal[plyMat[medSet, "team"]] <- plyMat[medSet, "heal"]
-  percentVec <- plyMat[, "hr"] / totHeal[plyMat[, "team"]]
-
-  numStreaks <- c(table(match$killstreaks$steamid))
-  streakVec <- rep(0, dim(plyMat)[1])
-  streakVec[match(names(numStreaks), rownames(plyMat))] <- numStreaks
+	plyMat <- t(vapply(niceMatch$players,function(x){
+		cleanLogMat(x, teamNames = names(match$teams))
+	},rep(1.1, 27)))
 
 
-  finNames <- c(colnames(plyMat), "daphr", "hr_ratio", "dmg_realpdmg", "num_streaks")
-  plyMat <- cbind(plyMat, dmgPerVec, percentVec, cleanUpVec, streakVec)
-  colnames(plyMat) <- finNames
+	cleanUpVec <- plyMat[, "dmg_real"] / plyMat[, "dmg"]
+	dmgPerVec <- plyMat[, "dmg"] / plyMat[, "hr"]
+	dmgPerVec[is.infinite(dmgPerVec)] <- NA #Medics don't get heals
 
-  if(length(altNames) > 0){
-    altNums <- match(names(altNames), rownames(plyMat))
-    if(any(is.na(altNums))){
-      stop(paste("These names did not have matches in this log:",
-      altNames[is.na(altNums)],
-      "Check the steam ID", sep = "/n"))
-    }
+	totHeal <- rep(0,2)
+	medSet <- names(match$healspread)
+	totHeal[plyMat[medSet, "team"]] <- plyMat[medSet, "heal"]
+	percentVec <- plyMat[, "hr"] / totHeal[plyMat[, "team"]]
 
-    newNames <- rownames(plyMat)
-    newNames[altNums] <- altNames
-  	rownames(plyMat) <- newNames
-  }
+	numStreaks <- c(table(match$killstreaks$steamid))
+	streakVec <- rep(0, dim(plyMat)[1])
+	streakVec[match(names(numStreaks), rownames(plyMat))] <- numStreaks
 
-  niceMatch$table <- plyMat
-  for(pind in 1:length(niceMatch$players)){
-    dupSet <- !is.na(match(names(niceMatch$players[[pind]]), colnames(plyMat)))
-    niceMatch$players[[pind]][dupSet] <- NULL
-  }
 
-  return(niceMatch)
+	finNames <- c(colnames(plyMat), "daphr", "hr_ratio", "dmg_realpdmg", "num_streaks")
+	plyMat <- cbind(plyMat, dmgPerVec, percentVec, cleanUpVec, streakVec)
+	colnames(plyMat) <- finNames
+
+	if(length(altNames) > 0){
+		altNums <- match(names(altNames), rownames(plyMat))
+		if(any(is.na(altNums))){
+			stop(paste("These names did not have matches in this log:",
+			altNames[is.na(altNums)],
+			"Check the steam ID", sep = "/n"))
+		}
+
+		newNames <- rownames(plyMat)
+		newNames[altNums] <- altNames
+		rownames(plyMat) <- newNames
+	}
+
+	niceMatch$table <- plyMat
+	for(pind in 1:length(niceMatch$players)){
+		dupSet <- !is.na(match(names(niceMatch$players[[pind]]), colnames(plyMat)))
+		niceMatch$players[[pind]][dupSet] <- NULL
+	}
+
+	return(niceMatch)
 
 
 }
 
 cleanLogMat <- function(lplayer, teamNames){
-  lplayer$team <- match(lplayer$team, teamNames)
-  simpleSet <- vapply(lplayer, function(x){ !is.list(x) && !is.data.frame(x) }, TRUE)
-  cropped <- as.numeric(lplayer[simpleSet])
-  names(cropped) <- names(lplayer[simpleSet])
-  return(cropped)
+	lplayer$team <- match(lplayer$team, teamNames)
+	simpleSet <- vapply(lplayer, function(x){ !is.list(x) && !is.data.frame(x) }, TRUE)
+	cropped <- as.numeric(lplayer[simpleSet])
+	names(cropped) <- names(lplayer[simpleSet])
+	return(cropped)
 }
 
 #' Do statistics on multiple logs
 #'
 #' @param lmatch a list of logs produced by \code{\link{getLog}}
 #' @param statFun The statistical function that you want to use on the logs.
-#'   Default is a simple mean, but can include any function, R or user made,
-#'   with a \code{na.rm} parameter
+#'	 Default is a simple mean, but can include any function, R or user made,
+#'	 with a \code{na.rm} parameter
 #'
 #' @return A matrix of the model appliled log table for all the participants
-#'   involved in any of the games. There is an extra column of \code{gp}, or
-#'   games played.
+#'	 involved in any of the games. There is an extra column of \code{gp}, or
+#'	 games played.
 #' @export
 aggregateStats <- function(lmatch, statFun = mean){
 
-  lPlayers <- lapply(lmatch, function(x){
-    rownames(x$table)
-  })
-  allPlayerNames <- unique(c(lPlayers, recursive = TRUE))
+	lPlayers <- lapply(lmatch, function(x){
+		rownames(x$table)
+	})
+	allPlayerNames <- unique(c(lPlayers, recursive = TRUE))
 
-  gamesPlayed <- vapply(allPlayerNames, function(nam){
-    didParticipate <- vapply(lPlayers, function(logPlys){
-      any(grepl(nam, logPlys))
-    }, TRUE)
-    sum(didParticipate)
-  }, 1)
+	gamesPlayed <- vapply(allPlayerNames, function(nam){
+		didParticipate <- vapply(lPlayers, function(logPlys){
+			any(grepl(nam, logPlys))
+		}, TRUE)
+		sum(didParticipate)
+	}, 1)
 
-  agg <- array(NA, dim=c(
-    length(allPlayerNames),
-    dim(lmatch[[1]]$table)[2],
-    max(gamesPlayed)))
+	agg <- array(NA, dim=c(
+		length(allPlayerNames),
+		dim(lmatch[[1]]$table)[2],
+		max(gamesPlayed)))
 
 
 
-  dimnames(agg)[[1]] <- allPlayerNames
-  dimnames(agg)[[2]] <- colnames(lmatch[[1]]$table)
+	dimnames(agg)[[1]] <- allPlayerNames
+	dimnames(agg)[[2]] <- colnames(lmatch[[1]]$table)
 
-  gpcnt <- rep(1, length(allPlayerNames))
-  for(log in lmatch){
-    aggInds <- match(rownames(log$table), allPlayerNames)
-    agg[aggInds, , gpcnt[aggInds]] <- log$table
+	gpcnt <- rep(1, length(allPlayerNames))
+	for(log in lmatch){
+		aggInds <- match(rownames(log$table), allPlayerNames)
+		agg[aggInds, , gpcnt[aggInds]] <- log$table
 
-    gpcnt[aggInds] <- gpcnt[aggInds] + 1
-  }
+		gpcnt[aggInds] <- gpcnt[aggInds] + 1
+	}
 
-  agg <- apply(X = agg, MARGIN = c(1, 2), FUN = statFun, na.rm = TRUE)
+	agg <- apply(X = agg, MARGIN = c(1, 2), FUN = statFun, na.rm = TRUE)
 
-  agg <- cbind(agg, gp = gamesPlayed)
+	agg <- cbind(agg, gp = gamesPlayed)
 
-  return(agg)
+	return(agg)
 }
--- R/queryLogstf.R
+++ R/queryLogstf.R
@@ -1,87 +1,87 @@
 queryLogstf <- function(
-  players = c(),
-  teamName = "",
-  season = "",
-  tournament = "",
-  shGetLog = TRUE){
+	players = c(),
+	teamName = "",
+	season = "",
+	tournament = "",
+	shGetLog = TRUE){
 
-  queries <- matrix(nrow = 1, ncol = 3)
-  colnames(queries) <- c("title", "uploader", "player")
-  ids <- c()
+	queries <- matrix(nrow = 1, ncol = 3)
+	colnames(queries) <- c("title", "uploader", "player")
+	ids <- c()
 
-  if(length(players) > 0){
-    playDict <- vapply(players, function(x){
-      tftvUser2SteamID(x)
-    }, "e")
-    addPlayerQs(queries) <- playDict
-  }
+	if(length(players) > 0){
+		playDict <- vapply(players, function(x){
+			tftvUser2SteamID(x)
+		}, "e")
+		addPlayerQs(queries) <- playDict
+	}
 
-  if(length(season) > 0){
-    addSeasonQs(queries) <- season
-  }
+	if(length(season) > 0){
+		addSeasonQs(queries) <- season
+	}
 
-  if(length(tournament) > 0){
-    ids <- c(ids, getLogIDsComptf(tournament, "Tourney"))
-  }
+	if(length(tournament) > 0){
+		ids <- c(ids, getLogIDsComptf(tournament, "Tourney"))
+	}
 
-  if(dim(queries)[1] + length(ids) == 1 ){
-    stop(paste("Please supply a list of",
-    "players, the team name or the tournament to query the logs"))
-  } else { queries <- queries[-1,] }
+	if(dim(queries)[1] + length(ids) == 1 ){
+		stop(paste("Please supply a list of",
+		"players, the team name or the tournament to query the logs"))
+	} else { queries <- queries[-1,] }
 
-  ids <- c(ids, getLogIDsJSON(
-    title = queries[, "title"],
-    uploader = queries[, "uploader"],
-    player = queries[, "player"],
-    num = 10))
+	ids <- c(ids, getLogIDsJSON(
+		title = queries[, "title"],
+		uploader = queries[, "uploader"],
+		player = queries[, "player"],
+		num = 10))
 
-  andIds <- notUnique(ids)
+	andIds <- notUnique(ids)
 
-  if(shGetLog){
-    llogs <- lapply(andIds, getLog, altNames = playDict)
-    return(llogs)
-  } else {
-    return(ids)
-  }
+	if(shGetLog){
+		llogs <- lapply(andIds, getLog, altNames = playDict)
+		return(llogs)
+	} else {
+		return(ids)
+	}
 
 }
 
 `addPlayerQs<-` <- function(qs, value){
-  return(rbind(qs, cbind("", "", value)))
+	return(rbind(qs, cbind("", "", value)))
 }
 
 tftvUser2SteamID <- function(tftvUserName){
-  tftvurl <- paste0("http://www.teamfortress.tv/user/", tftvUserName)
-  xpathIDs <- '//*[@id="content-inner"]/div[1]/table[1]'
+	tftvurl <- paste0("http://www.teamfortress.tv/user/", tftvUserName)
+	xpathIDs <- '//*[@id="content-inner"]/div[1]/table[1]'
 
-  node <- easyScrape(tftvurl, xpathIDs, "Page Not Found")
-  tftable <- rvest::html_table(node)
-  sid3 <- tftable[tftable[,1] == "SteamID3", 2]
+	node <- easyScrape(tftvurl, xpathIDs, "Page Not Found")
+	tftable <- rvest::html_table(node)
+	sid3 <- tftable[tftable[,1] == "SteamID3", 2]
 
-  names(sid3) <- tftvUserName
-  return(sid3)
+	names(sid3) <- tftvUserName
+	return(sid3)
 }
 
 convSID2name <- function(iden){
-  query <- paste0("http://steamcommunity.com/profiles/", iden)
-  realProfile <- twitteR::decode_short_url(query)
-  return(gsub("http://steamcommunity.com/id/", "", realProfile))
+	query <- paste0("http://steamcommunity.com/profiles/", iden)
+	realProfile <- twitteR::decode_short_url(query)
+	return(gsub("http://steamcommunity.com/id/", "", realProfile))
 }
 
 easyScrape <- function(url, xpathCap, failRegex){
-  htmlFile <- xml2::read_html(url)
-  if( grepl(failRegex, htmlFile) ){
-  	stop(paste0("'", value, "'", " is not a real web page/search (404)"))
-  }
-  node <- rvest::html_node(htmlFile, xpath = xpathCap)
-  return(node)
+	htmlFile <- xml2::read_html(url)
+	if( grepl(failRegex, htmlFile) ){
+		stop(paste0("'", value, "'", " is not a real web page/search (404)"))
+	}
+	node <- rvest::html_node(htmlFile, xpath = xpathCap)
+	return(node)
 }
 
 `addSeasonQs<-` <- function(qs, value){
-  #Go through league uploads
+	#Go through league uploads
 }
 
 notUnique <- function(vec){
-  dupSet <- duplicated(vec) | duplicated(vec, fromLast = TRUE)
-  return(unique(vec[dupSet]))
+	dupSet <- duplicated(vec) | duplicated(vec, fromLast = TRUE)
+	return(unique(vec[dupSet]))
 }
