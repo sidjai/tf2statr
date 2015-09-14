aab903f20ba6bbc61c7d0ca09330116cbd93e7a0
Siddarta Jairam <sidjsb@gmail.com>
2015-09-14 11:49:25 -0700

    added data checks + rvest update
    - rvest is getting a major update by changing from XML package to the xml2 package that uses a c++ lib. It also comes with a lot of depreciated functions. This happened in v 0.3.0 but only 0.2.0 is up on CRAN.
    -- There is enough changes to the package and to mey package if I would downgrade to keep the development version requirement.

--- README.md
+++ README.md
@@ -7,6 +7,13 @@ Tracking tools for TF2 matches have matured to the point that more detailed expl
 ## Installation
 
 ```r
-install.packages("devtools")
+#install.packages("devtools")
 devtools::install_github("sidjai/tf2statr")
+```
+As a headsup, the package depends on rvest v 0.3.0 which at the moment can only be downloaded in the development version on github as follows
+
+```r
+# install.packages("devtools")
+install_github("hadley/rvest")
+
 ```
--- tests/testthat/test-archive.R
+++ tests/testthat/test-archive.R
@@ -3,3 +3,27 @@ context("Archiving")
 test_that("Get name archives", {
 	expect_true(grepl("[U:1:2431135]", getArchiveSIDfromName("ryb")))
 })
+
+test_that("playerDict archive is healthy", {
+	data("playerDict")
+
+	playerDict <- as.matrix(playerDict)
+	expect_true(is.character(playerDict))
+	expect_false(any(grepl("\\[", playerDict[, 1])))
+	expect_true(all(grepl("\\[", playerDict[, 2])))
+
+})
+
+test_that("eventArchive data set is healthy", {
+	data("eventArchive")
+
+	expect_true(is.list(events))
+
+	eleList <- vapply(events, is.list, TRUE)
+	expect_true(all(eleList))
+
+	eleNames <- vapply(events, function(x){
+		!is.null(names(x)) && all(nzchar(x))
+	}, TRUE)
+	expect_true(all(eleNames))
+})
